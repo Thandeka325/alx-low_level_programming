54d9160b8a9043e1d8aeb14ebf652e62a91b433e
Thandeka325 <marhwayiza325@gmail.com>
2024-06-20 12:10:31 -0700

    Task 8. UNIX is basically a simple operating system, but you have to be a genius to understand the simplicity

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
  * main - Entry point
@@ -8,6 +9,6 @@
 int main(void)
 {
 	write(STDOUT_FILENO, "and that piece of art is useful
-		       \" - Dora Korpar, 2015-10-19\n");
+		       \" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
