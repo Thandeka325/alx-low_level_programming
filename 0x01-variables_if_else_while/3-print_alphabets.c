056bff5e7b34d3c7368a7a3e2f2f71b51a9981a1
Thandeka325 <marhwayiza325@gmail.com>
2024-06-21 07:18:39 -0700

    Task 3. alphABET

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Print the alphabets in lowercase,and uppercase
+ *
+ * Return: 0 (Sucess)
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; letter <= 'z'; c++)
+	{
+		putchar(c);
+	}
+	
+	for (c = 'A'; c <= 'Z'; c++)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+
+	return (0);
+}
