9a01bdd4c483995e6c8c135958c6626c36b1bcff
Thandeka325 <marhwayiza325@gmail.com>
2024-06-21 12:24:04 -0700

    Task 12. Software is eating the World

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,16 +1,17 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
  * main - Print all possible combinations of two-digit numbers
  * Numbers range from 0 to 99,two numbers separated by a space
  * Printed with 2 digits
  * Combination separated by comma and a space,ascending order
- * print_number - Function that prints a two-digit.
+ * printnumber - Function that prints a two-digit.
  * @n: the number to print
- * Description: can only use _putchar to print.
+ * Description: can only use putchar to print
  * Return: 0 (Success)
  */
-void print_number(int n)
+void printnumber(int n)
 {
 	putchar((n / 10) + '0');
 	putchar((n % 10) + '0');
@@ -24,9 +25,9 @@ int main(void)
 	{
 		for (num2 = num1 + 1; num2 <= 99; num2++)
 		{
-			print_number(num1);
+			printnumber(num1);
 			putchar(' ');
-			print_number(num2);
+			printnumber(num2);
 
 			if (!(num1 == 98 && num2 == 99))
 			{
