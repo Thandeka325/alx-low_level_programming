dfdc66f0fa9fdfe71d6533f46e4a48dd140537aa
Thandeka325 <marhwayiza325@gmail.com>
2024-06-21 11:24:56 -0700

    Task 12. Software is eating the World

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+/**
+ * main - Print all possible combinations of two-digit numbers
+ * Numbers range from 0 to 99,two numbers separated by a space
+ * Printed with 2 digits
+ * Combination separated by comma and a space,ascending order
+ *
+ * Return: 0 (Success)
+ */
+void print_number(int n)
+{
+	putchar((n / 10) + '0');
+	putchar((n % 10) + '0');
+}
+
+int main(void)
+{
+	int num1, num2;
+
+	for (num1 = 0; num1 <= 99; num1++)
+	{
+		for (num2 = num1 +1; num2 <= 99; num2++)
+		{
+			print_number(num1);
+			putchar(' ');
+			print_number(num2);
+
+			if (!(num1 == 98 && num2 == 99))
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}
