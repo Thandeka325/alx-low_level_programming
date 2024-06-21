a316d967614b139f59859fe5744c29f3c9939ab4
Thandeka325 <marhwayiza325@gmail.com>
2024-06-21 09:02:49 -0700

    Task 6. Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Print all single digit numbers of base 10 from 0, without using char
+ *
+ * for loop iterates over the integers from 0 to 9
+ *
+ * inside the loop,the integer is converted to its corresponding character by adding the ASCII value of 0
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		putchar(i + '0');
+	}
+
+	putchar('\n');
+
+	return (0);
+}
