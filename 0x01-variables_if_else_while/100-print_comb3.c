4b518121d0bf790723808a8c8b1f014971a57ee8
Thandeka325 <marhwayiza325@gmail.com>
2024-06-21 10:29:12 -0700

    Task 10. Inventing is a combination of brains and materials. The more brains you use, the less material you need

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+ * main - Print all possible different combinations of two digits
+ * Comma and space separated, two digits must be different
+ * 01 and 10 considered same combo of 0 and 1
+ * Print only the smallest combinations
+ * Be in ascending order
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	int x, y;
+
+	for (x = 0; x <=8; x++)
+	{
+		for (y = x + 1; y <= 9; y++)
+		{
+			putchar(x + '0');/*print the first digit*/
+			putchar(y + '0');/*print second digit*/
+
+			if (x != 8 || y != 9)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}
