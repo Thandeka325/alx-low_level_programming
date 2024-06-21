748896853a8881fb0296700bfe7e0847a172a322
Thandeka325 <marhwayiza325@gmail.com>
2024-06-21 10:07:19 -0700

    Task 9. Patience, persistence and perspiration make an unbeatable combination for success

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - Print all possible combinations of single-digit numbers 
+ * separated by comma and followed by a space
+ * for loop iterates over integers from 0 to 9 and convert to corresponding char
+ * if function is set to print comma and a space if digit is not 9
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	int y;
+
+	for (y = 0; y <= 9; y++)
+	{
+		putchar(y + '0');/*print the digit*/
+		if (y != 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}
