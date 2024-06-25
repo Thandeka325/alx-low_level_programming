daa22bee3bfb981d51eb80de45ceb4c9f3417946
Thandeka325 <marhwayiza325@gmail.com>
2024-06-25 15:07:53 -0700

    Task 12. The World looks like a multiplication-table, or a mathematical equation, which, turn it how you will, balances itself

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -6,27 +6,28 @@
  * @n: Number to print times table for
  * Takes integer 'n' as input
  * Checks if 'n'is within valid range (0 to 15)
- * If true iterates through nested 'for' loop generates times table
- * Prints each product 'i * j'using 'printf' with '%4d' format
- * The above ensures each num occupies at least 4 characters
+ * 'for' loops iterare through rows(i) & columns(j)
+ * It calculates the product 'i * j'
+ * Prints it formatted o ensure proper alignment(%2d and %4d)
  * If 'n' is > 15, < 0 function should not print anything
  *
  */
 void print_times_table(int n)
 {
 	if (n < 0 || n > 15)
-	{
 		return;
-	}
 
 	int i, j;
 
-	for (i = 0; i <=n; i++)
+	for (i = 0; i <= n; i++)
 	{
 		for (j = 0; j <= n; j++)
 		{
-			printf("%4d", i * j);
+			if (j == 0)
+				printf("%2d", i * j);
+			else
+				printf("%4d", i * j);
 		}
-		print("\n");
+		printf("\n");
 	}
 }
