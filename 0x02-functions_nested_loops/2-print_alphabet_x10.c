5ceabc8c419f6bb8f335ba94f714fc5cd59160a6
Thandeka325 <marhwayiza325@gmail.com>
2024-06-25 05:57:30 -0700

    Task 2. 10 x alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+include "main.h"
+
+/**
+ * print_alphabet_x10 - Entry point
+ *
+ * Description: prints 10x the alphabet, in lowercase
+ *
+ * Return: Always 0 (success)
+ */
+void print_alphabet_x10(void)
+{
+	char alphabet[] = "abcdefghijklmnopqrstuvwxyz";
+	int i, j;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; alphabet[j] != '\0'; j++)/**nested loop, runs 10x, inner loop iterates over alphabet string*/
+		{
+			_putchar(alphabet[j]);
+		}
+		_putchar('\n');
+	}
+}
