e4b0b009e57eed75a4bd802b69a787f09f02b1fd
Thandeka325 <marhwayiza325@gmail.com>
2024-06-25 13:46:46 -0700

    Task 11. 98 Battery Street, the OG

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_to_98 - Prints all natural numbers from n to 98
+ * @n: The starting number
+ * Takes integer 'n' as input
+ * Checks if 'n' is <= 98
+ * If above check is true loop iterates from n to 98
+ * Prints each number followed by comma & space
+ * If false (n > 98) iterates from 'n' down to 98
+ */
+void print_to_98(int n);
+{
+	int i;
+
+	if (n <= 98)
+	{
+		for (i = n; i <= 98; i++)
+		{
+			printf("%d", i);
+
+			if (i != 98)
+			{
+				printf(", ");
+			}
+		}
+	}
+	else 
+	{
+		for (i = n; i >=98; i--)
+		{
+			printf("%d", i);
+
+			if (i != 98)
+			{
+				printf(", ");
+			}
+		}
+	}
+	printf("\n");
+}
