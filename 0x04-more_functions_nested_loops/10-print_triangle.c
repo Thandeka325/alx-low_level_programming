5225f784538cb7685ad3881f9ea7b21756a807fd
Thandeka325 <marhwayiza325@gmail.com>
2024-06-27 12:06:01 -0700

    Task 10. Triangles-Write a function that prints a triangle

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -11,20 +11,26 @@
  */
 void print_triangle(int size)
 {
-	int i, j;
-
 	if (size <= 0)
 	{
 		_putchar('\n');
 	}
 	else
 	{
+		int i, j;
+
 		for (i = 1; i <= size; i++)
 		{
-			for (j = 1; j <= i; j++)
+			for (j = i; j < size; j++)
+			{
+				_putchar(' ');
+			}
+
+			for (j = 1; j <= i; j++);
 			{
 				_putchar('#');
 			}
+
 			_putchar('\n');
 		}
 	}
