d1537850990c0db7730c37bc10b1b090b1b8b698
Thandeka325 <marhwayiza325@gmail.com>
2024-06-27 12:20:25 -0700

    Task 10. Triangles-Write a function that prints a triangle

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -1,27 +1,33 @@
 #include "main.h"
 
 /**
- * print_triangle - Prints a triangle
- * @size: Is the size of the triangle
- * If size is 0 or less, print only a new line
+ * print_triangle - prints a triangle, followed by a new line
+ * @size: size of the triangle
+ * If size is 0 or less,print only a new line
  * Use the character # to print the triangle
  */
 void print_triangle(int size)
 {
-	int i, j;
-
 	if (size <= 0)
 	{
 		_putchar('\n')
 	}
 	else
 	{
+		int i, j;
+
 		for (i = 1; i <= size; i++)
 		{
+			for (j = i; j < size; j++)
+			{
+				_putchar(' ');
+			}
+
 			for (j = 1; j <= i; j++)
 			{
 				_putchar('#');
 			}
+
 			_putchar('\n');
 		}
 	}
