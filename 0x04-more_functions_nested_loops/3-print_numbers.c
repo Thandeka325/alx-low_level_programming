f089b8b870419bf3283674a684a3a972130d4e5c
Thandeka325 <marhwayiza325@gmail.com>
2024-06-27 09:22:41 -0700

    Task 4. I believe in numbers and signs-Write a function that prints the numbers, from 0 to 9, followed by a new line.

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -7,10 +7,10 @@
  */
 void print_numbers(void)
 {
-	char c;
+	int num;
 
-	for (c >= '0'; c <= '9'; c++)
-		_putchar(c);
+	for (num = '0'; num <= '9'; num++)
+		_putchar(num + '0');
 
 	_putchar('\n');
 }
