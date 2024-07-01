8dd3e353a5c3335c2f7a28fb2e53c6f6dbc2e8f4
Thandeka325 <marhwayiza325@gmail.com>
2024-07-01 14:10:18 -0700

    Task 8. Arrays are not pointers-Write a function that prints n elements of an array of integers, followed by a new line.

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_array - prints n elements of an array of integers
+ * @a: The array of integers
+ * @n: The number of elements of the array 
+ * Numbers must be separated by comma and space,
+ * be displayed in the same order as they are stored
+ * for loop iterates 4rm 1st element to the 'n'-th element
+ * if statement ensures that comma and space are printed
+ *
+ */
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", a[i]);
+		if (i < n - 1)
+		{
+			printf(", ");
+		}
+	}
+	printf("\n");
+}
