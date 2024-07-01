b53e294457fca1cdd787be7afd0a113bf59be477
Thandeka325 <marhwayiza325@gmail.com>
2024-07-01 14:48:15 -0700

    Task 10. Write a function that convert a string to an integer

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -14,28 +14,38 @@
  */
 int _atoi(char *s)
 {
-	int i = 0;
-	int sign = 1;
-	int result = 0;
-	int has_started = 0;
+	int i, d, n, len, f, digit;
 
-	while (s[i] != '\0')
+	i = 0;
+	d = 0;
+	n = 0;
+	len = 0;
+	f = 0;
+	digit = 0;
+
+	while (s[len] != '\0')
+		len++;
+
+	while (i < len && f == 0)
 	{
-		if (s[i] == '-' && !has_started)
-		{
-			sign = -sign;
-		}
-		else if (s[i] >= '0' && s[i] <= '9')
-		{
-			result = result * 10 + (s[i] - '0');
-			has_started = 1;
-		}
-		else if (has_started)
-		{
-			break;
-		}
-		i++;
-	}
+		if (s[i] == '-')
+			++d;
 
-	return (sign * result);
+		if (s[i] >= '0' && s[i] <= '9')
+		{
+			digit = s[i] - '0';
+			if (d % 2)
+				digit = -digit;
+			n = n * 10 + digit;
+			f = 1;
+			if (s[i + 1] < '0' || s[i] > '9')
+				break;
+			f = 0;
+		}
+		i++
+	}
+	if (f == 0)
+		return (0);
+
+	return (n);
 }
