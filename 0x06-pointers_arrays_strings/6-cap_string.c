07a5e84ac6ba154a52c9ebbc9a971182ce423ce7
Thandeka325 <marhwayiza325@gmail.com>
2024-07-03 15:10:07 -0700

    Task 6 Write a function that capitalizes all words of a string.

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -2,38 +2,34 @@
 
 /**
  * cap_string - capatalizes every word of a string
- * @str: string too be modified
+ * @str: string to be capitalized
  *
  * Return: the resulting string
  */
 char *cap_string(char *str)
 {
-	int i, c;
-	int trigger;
-	char nots[] = ",;.!?(){}\n\t\"";
+	int index = 0;
 
-	for (i = 0; trigger = 0; str[i] != '\0'; i++)
+	while (str[++index])
 	{
-		if (str[0] > 96 && str[0] < 123)
-			trigger = 1;
-		for (c = 0; nots[c] != '\0'; c++)
-		{
-			if (nots[c] == str[i])
-				trigger = 1;
-		}
+		while (!(str[index] >= 'a' && str[index] <= 'z'))
+			index++;
 
-		if (trigger)
-		{
-			if (str[i] > 96 && str[i] < 123)
-			{
-				str[i] -= 32;
-				trigger = 0;
-			}
-			else if (str[i] > 64 && str[i] < 91)
-				trigger = 0;
-			else if (str[i] > 47 && str[i] < 58)
-				trigger = 0;
-		}
+		if ((str[index - 1] == ' ' ||
+				str[index - 1] == '\t' ||
+				str[index - 1 == '\n' ||
+				str[index - 1 == ',' ||
+				str[index - 1 == ';' ||
+				str[index - 1 == '.' ||
+				str[index - 1 == '!' ||
+				str[index - 1 == '?' ||
+				str[index - 1 == '"' ||
+				str[index - 1 == '(' ||
+				str[index - 1 == ')' ||
+				str[index - 1 == '{' ||
+				str[index - 1 == '}')
+			str[index] -= 32;
 	}
 	return (str);
 }
+
