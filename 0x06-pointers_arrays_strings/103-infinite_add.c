f9a99b0ac8436416db46ca9d73b4ac6af9f660bd
Thandeka325 <marhwayiza325@gmail.com>
2024-07-03 16:35:23 -0700

    Task 11. Write a function that adds two numbers.

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -19,7 +19,7 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 	for (j = 0; n2[j]; j++)
 		;
 
-	if (i > size_r || j > size_r)
+	if (i >= size_r || j >= size_r)
 		return (0);
 
 	m = 0;
@@ -31,9 +31,8 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 		if (j >= 0)
 			n += n2[j] - '0';
 		if (i < 0 && j < 0 && n == 0)
-		{
 			break;
-		}
+
 		m = n / 10;
 		r[k] = n % 10 + '0';
 	}
