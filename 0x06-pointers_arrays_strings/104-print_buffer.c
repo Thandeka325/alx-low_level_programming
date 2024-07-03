53500c7e6471406f5d0ff6eb85c8c2b3bf2fff77
Thandeka325 <marhwayiza325@gmail.com>
2024-07-03 15:33:44 -0700

    Task 12. Write a function that prints a buffer.

---
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -0,0 +1,52 @@
+#include "main.h"
+
+/**
+ * print_buffer - prints a buffer
+ * @size: size of byte
+ * @b: number of bytes
+ * The output should print 10 bytes per line
+ * starts with position of first byte in hexadecimal (8 chars)
+ * starting with '0'
+ * shows the hexadecimal content (2 chars) of the buffer
+ * '2' bytes at a time, separated by a space
+ * shows the content of the buffer,
+ *  If byte printable character, print the letter, if not, print'.'
+ * Each line ends with a new line \n
+ * If size is 0 or less, the output should be a new line only \n
+ */
+void print_buffer(char *b, int size)
+{
+	int i = 0, j;
+
+	if (size < 0)
+	{
+		printf('\n');
+		return;
+	}
+
+	while (i < size)
+	{
+		if (i % 10 == 0)
+			printf("%08x: ", i);
+		for (j = i; j < i + 9; j += 2)
+		{
+			if ((j < size) && ((j + 1) < size))
+				printf("%02x%02x: ", b[j], b[j + 1]);
+			else
+			{
+				while (++j <= i + 10)
+					printf(" ");
+				printf(" ");
+			}
+		}
+		for (j = i; j < i + 9 && j < size; j++)
+		{
+			if (b[j] >= 32 && b[j] <= 126)
+				printf("%c", b[j]);
+			else
+				print(".");
+		}
+		printf('n');
+		i += 10;
+	}
+}
