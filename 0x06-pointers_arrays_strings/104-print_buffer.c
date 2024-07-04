a252e510c83050c3ab9795484c3ab01a305f6286
Thandeka325 <marhwayiza325@gmail.com>
2024-07-04 12:51:32 -0700

    Task 12. Write a function that prints a buffer.

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -5,15 +5,14 @@
  * print_buffer - prints a buffer
  * @b: buffer to be printed
  * @size: number of bytes to be printed from the buffer
- * The output should print 10 bytes per line
- * starts with the position of the first byte in hexadecimal (8 chars)
- * starting with '0'
- * shows the hexadecimal content (2 chars) of the buffer
- * '2' bytes at a time, separated by a space
- * shows the content of the buffer,
- *  If byte is a printable character, print the letter, if not, print '.'
- * Each line ends with a new line \n
- * If size is 0 or less, the output should be a new line only \n
+ *
+ * Description: The output should print 10 bytes per line.
+ * Each line starts with the position of the first byte in hexadecimal (8 chars),
+ * starting with '0'. It shows the hexadecimal content (2 chars) of the buffer,
+ * 2 bytes at a time, separated by a space. It also shows the content of the
+ * buffer, printing printable characters as they are and replacing non-printable
+ * characters with '.'. Each line ends with a newline.
+ * If the size is 0 or less, the output is just a newline.
  */
 void print_buffer(char *b, int size)
 {
