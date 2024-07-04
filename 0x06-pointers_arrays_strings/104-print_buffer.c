30ca3b89b55d3b0b1007cf5e611921b1cf7e286a
Thandeka325 <marhwayiza325@gmail.com>
2024-07-04 12:44:41 -0700

    Task 12. Write a function that prints a buffer.

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -3,54 +3,53 @@
 
 /**
  * print_buffer - prints a buffer
- * @size: size of byte
- * @b: number of bytes
+ * @b: buffer to be printed
+ * @size: number of bytes to be printed from the buffer
  * The output should print 10 bytes per line
- * starts with position of first byte in hexadecimal (8 chars)
+ * starts with the position of the first byte in hexadecimal (8 chars)
  * starting with '0'
  * shows the hexadecimal content (2 chars) of the buffer
  * '2' bytes at a time, separated by a space
  * shows the content of the buffer,
- * If byte printable character, print the letter, if not, print'.'
+ *  If byte is a printable character, print the letter, if not, print '.'
  * Each line ends with a new line \n
  * If size is 0 or less, the output should be a new line only \n
  */
 void print_buffer(char *b, int size)
 {
-	int i, j;
+    int i, j;
 
-	if (size <= 0)
-	{
-		printf("\n");
-		return;
-	}
-	for (i = 0; i < size; i += 10)
-	{
-		printf("%08x: ", i);
+    if (size <= 0)
+    {
+        printf("\n");
+        return;
+    }
 
-		for (j = 0; j < 10; j++)
-		{
-			if ((i + j) < size)
-				printf("%02x", b[i + j]);
-			else
-				printf(" ");
-			if (j % 2)
-				printf(" ");
-		}
+    for (i = 0; i < size; i += 10)
+    {
+        printf("%08x: ", i);
 
-		for (j = 0; j < 10; j++)
-		{
-			if ((i + j) < size)
-			{
-				char c = b[i + j];
+        for (j = 0; j < 10; j++)
+        {
+            if ((i + j) < size)
+                printf("%02x", b[i + j]);
+            else
+                printf("  ");
+            if (j % 2)
+                printf(" ");
+        }
 
-				if (c >= 32 && c <= 126)
-					printf("%c", c);
-				else
-					printf(".");
-			}
-		}
-		printf("\n");
-	}
+        for (j = 0; j < 10; j++)
+        {
+            if ((i + j) < size)
+            {
+                char c = b[i + j];
+                if (c >= 32 && c <= 126)
+                    printf("%c", c);
+                else
+                    printf(".");
+            }
+        }
+        printf("\n");
+    }
 }
-
