57d115bbbc0c6c186d8cb30b198c32962540008d
Thandeka325 <marhwayiza325@gmail.com>
2024-07-10 06:15:41 -0700

    Task 8. Write a function that compares two strings and returns 1 if the strings can be considered identical, otherwise return 0

---
+++ 0x08-recursion/101-wildcmp.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * wildcmp - Compares two strings
+ * @s1: The first string
+ * @s2: The second string
+ * '@s2' can contain the special character *.
+ * The special char * can replace any string(including an empty string)
+ *
+ * Return: 1 if the strings are identical, 0 otherwise
+ */
+int wildcmp(char *s1, char *s2)
+{
+	if (*s2 == '\0')
+		return (*s1 == '\0');
+	if (*s2 == '*')
+	{
+		if (*(s2 + 1) == '\0' || wldcmp(s1, s2 + 1))
+			return(1);
+		if (*s1 != '\0' && wildcmp(s1 + 1, s2))
+			return (1);
+	}
+	else if (*s1 == *s2)
+	{
+		return (wildcmp(s1 + 1, s2 + 1));
+	}
+	return (0);
+}
