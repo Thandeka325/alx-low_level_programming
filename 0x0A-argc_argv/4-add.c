19332098e086e91b7f71ae215923fe7519a9443f
Thandeka325 <marhwayiza325@gmail.com>
2024-07-12 17:09:13 -0700

    Task 4. Write a program that adds positive numbers.

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+/**
+ * main - Adds positive numbers
+ * @argc: Argument count
+ * @argv: Argument vector (array of strings)
+ * Print the result, followed by a new line
+ * If no number is passed to the program, print 0, followed by a new line
+ * If one of the number contains symbols that are not digits,
+ * print Error, followed by a new line, and return 1
+ *
+ * Return: 0 on Success, 1 on error
+ */
+int main(int argc, char *argv[])
+{
+	int i, j;
+	int sum = 0;
+
+	if (argc == 1)
+	{
+		printf("0\n");
+		return (0);
+	}
+
+	for (i = 1; i < argc; i++)
+	{
+		for (j = 0; argv[i][j] != '\0'; j++)
+		{
+			if (!isdigit(argv[i][j]))
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+		sum += atoi(argv[i]);
+	}
+
+	printf("%d\n", sum);
+	return (0);
+}
