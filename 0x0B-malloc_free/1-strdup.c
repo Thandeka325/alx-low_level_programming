ccc19d220d1411b7db0312e78b6b5ba90aab3545
Thandeka325 <marhwayiza325@gmail.com>
2024-07-15 05:26:17 -0700

    Task 1. Write a function that returns a pointer to a newly allocated space in memory, which contains a copy of the string given as a parameter

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _strdup - Returns a pointer to a newly allocated space in memory,
+ * which contains a copy of a string given as a parameter
+ * @str: The string to be duplicated.
+ * Memory for the new string is obtained with malloc, & freed with free
+ * 
+ * Return: Apointer to the duplicated string, or NULL if str is Null
+ * or if insufficient memory was available
+ */
+char *_strdup(char *str)
+{
+	char *dup;
+	unsigned int i, len;
+
+	if (str == NULL)
+		return (NULL);
+
+	for (len = 0; str[len] != '\0'; len++)
+		;
+
+	dup = malloc((len + 1) * sizeof(char));
+	if (dup == NULL)
+		return (NULL);
+
+	for (i = 0; i < len; i++)
+		dup[i] = str[i];
+	dup[len] = '\0';
+
+	return (dup);
+}
