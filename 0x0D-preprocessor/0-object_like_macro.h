b59cfe12394314fa9d80878867df498979f15351
Thandeka325 <marhwayiza325@gmail.com>
2024-07-22 05:12:54 -0700

    Task 0. Create a header file that defines a macro named SIZE as an abbreviation for the token 1024.

---
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -0,0 +1,11 @@
+#ifndef OBJECT_LIKE_MACRO_H
+#define OBJECT_LIKE_MACRO_H
+
+/**
+ * File: 0-object_like_macro.h
+ * Description: Header file that defines a macro name
+ * SIZE as an abbreviation for token 1024
+ */
+#define SIZE 1024
+
+#endif /* OBJECT_LIKE_MACRO_H */
