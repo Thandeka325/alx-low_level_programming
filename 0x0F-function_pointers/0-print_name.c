2b39cbef8e2dd7adabddf392d9ec1965fe8c63b3
Thandeka325 <marhwayiza325@gmail.com>
2024-07-24 03:36:45 -0700

    Task 0. Write a function that executes a function given as a parameter on each element of an array.

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,21 @@
+#include "function_pointers.h"
+#include <stdio.h>
+
+/**
+ * print_name - Prints a name using a given function
+ * @name: The name to be printed.
+ * @f: A pointer to a function that takes a char pointer as argument
+ *
+ * Description: This function prints the name provided to it
+ * using the function pointed to by @f. If either @name or @f
+ * is NULL, the function does nothing.
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	/* Check if name and function pointer are not NULL */
+	if (name == NULL || f == NULL)
+		return;
+
+	/* Call the function pointed to by f and pass the name */
+	f(name);
+}
