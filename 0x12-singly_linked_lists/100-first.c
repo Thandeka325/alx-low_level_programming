360c7b93b4c71fc709923effe4b09bfe5a0ff345
Thandeka325 <marhwayiza325@gmail.com>
2024-08-01 11:27:22 -0700

    Task 5. Write a function that prints text before executing the main function

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -5,6 +5,6 @@
  */
 void __attribute__((constructor)) before_main(void)
 {
-	printf("you're beat! and yet, you must allow,\n");
+	printf("You're beat! and yet, you must allow,\n");
 	printf("I bore my house upon my back!\n");
 }
