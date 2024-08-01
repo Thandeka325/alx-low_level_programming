d173415756a1b7a68e1d83b83d4083b750fc0b89
Thandeka325 <marhwayiza325@gmail.com>
2024-08-01 11:18:38 -0700

    Task 5. Write a function that prints text before executing the main function

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,13 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * print_before_main - Prints a specific message,
+ * before the main function.
+ *
+ */
+void print_before_main(void)
+{
+	printf("you're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
