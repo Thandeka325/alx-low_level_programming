abc1969a2e6f5a5165ed5bcd035f3b169737ac77
Thandeka325 <marhwayiza325@gmail.com>
2024-08-06 07:27:27 -0700

    Task 12. Function that prints a safe version listint_t linked list

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -6,24 +6,25 @@
  * print_listint_safe - Prints a listint_t linked list safely
  * @head: Pointer to the head of the list
  *
- * Return: The number of nodes in the list
+ * Return: The number of nodes in the list.If fails,exit with status 98
  */
 size_t print_listint_safe(const listint_t *head)
 {
 	const listint_t *current;
 	const listint_t *hold;
-	size_t count = 0;
-
-	if (head == NULL)
-		exit(98);
+	size_t count;
 
 	current = head;
 
+	if (current == NULL)
+		exit(98);
+
+	count = 0;
+
 	while (current != NULL)
 	{
-		printf("[%p] %d\n", (void *)current, current->n);
+		printf("[%p] %d\n", (void *)hold, hold->n);
 		count++;
-
 		hold = current;
 		current = current->next;
 
