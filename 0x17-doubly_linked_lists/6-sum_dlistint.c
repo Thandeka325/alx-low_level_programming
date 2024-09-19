244f76cf4ca9d6d3d28496afb24667ca1eea3c7b
Thandeka325 <marhwayiza325@gmail.com>
2024-09-19 06:25:20 -0700

    Task 6: Sum list

--- 0x17-doubly_linked_lists/6-sum_dlistint.c
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -3,9 +3,7 @@
 /**
  * sum_dlistint - Returns the sum of all the data (n) 
  * of a dlistint_t linked list.
- *
- * @head: Head of list
- *
+ * @head: Head of dlistint_t list
  * Return: Sum of the data, or 0 if the list is empty
  */
 int sum_dlistint(dlistint_t *head)
@@ -14,7 +12,6 @@ int sum_dlistint(dlistint_t *head)
 	dlistint_t *temp;
 
 	temp = head;
-
 	for (sum = 0; temp; temp = temp->next)
 		sum += temp->n;
 
