5ef97b5c1cbd0b3a30c90fb8675750926a7e8dac
Thandeka325 <marhwayiza325@gmail.com>
2024-09-19 06:38:43 -0700

    Task 7: Insert at index

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -10,39 +10,39 @@
  */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	unsigned int c;
-	dlistint_t *temp, *prev, *new;
+	dlistint_t *new;
+	dlistint_t *head;
+	unsigned int i;
 
-	new = malloc(sizeof(dlistint_t));
-	if (new == NULL)
-		return (NULL);
-	new->n = n;
-	for (temp = *h, c = 1; temp && c < idx; c++, temp = temp->next)
-		prev = temp;
+	new = NULL;
 	if (idx == 0)
+		new = add_dnodeint(h, n);
+	else
 	{
-		*h = new; new->prev = NULL;
-		new->next = (temp == NULL) ? NULL : temp;
-		return (new); }
-	if (idx == 1)
-	{
-		prev = *h;
-		temp = ((*h)->next == NULL) ? NULL : (*h)->next;
-		new->prev = prev; new->next = temp; prev->next = new;
-		if (temp)
-			temp->prev = new;
-		return (new); }
-	if (idx == c && temp == NULL)
-	{
-		if (prev != NULL)
+		head = *h;
+		i = 1;
+		if (head != NULL)
+			while (head->prev != NULL)
+				head = head->prev;
+		while (head != NULL)
 		{
-			new->prev = prev; new->next = NULL;
-			prev->next = new; return (new); }
-		free(new); return (NULL); }
-	else if (idx != c && temp == NULL)
-	{
-		free(new); return (NULL); }
-	prev = temp; temp = temp->next; new->prev = prev;
-	new->next = temp; prev->next = new; temp->prev = new;
+			if (i == idx)
+			{
+				if (head->next == NULL)
+					new = add_dnodeint_end(h, n);
+				else
+				{
+					new = malloc(sizeof(dlistint_t));
+					if (new != NULL)
+					{
+						new->n = n;
+						new->next = head->next;
+						new->prev = head;
+						head->next->prev = new;
+						head->next = new; }
+				}
+				break; }
+			head = head->next;
+			i++; }}
 	return (new);
 }
