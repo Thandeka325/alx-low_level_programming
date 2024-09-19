9e26c4111615702da0e42a3ce8264c7ef3565539
Thandeka325 <marhwayiza325@gmail.com>
2024-09-19 05:52:09 -0700

    Task 6: Sum list

---
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,30 @@
+#include "lists.h"
+
+/**
+ * sum_dlistint - Returns the sum of all the data (n) 
+ * of a dlistint_t linked list.
+ *
+ * @head: Head of list
+ *
+ * Return: Sum of the data
+ */
+int sum_dlistint(dlistint_t *head)
+{
+	int sum;
+
+	sum = 0;
+
+	if (head != NULL)
+	{
+		while (head->prev != NULL)
+			head = head->prev;
+
+		while (head != NULL)
+		{
+			sum += head->n;
+			head = head->next;
+		}
+	}
+
+	return (sum);
+}
