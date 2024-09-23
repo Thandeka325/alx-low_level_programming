1086acf73c1df2ad9a4a53663ef3bbeaff512bda
Thandeka325 <marhwayiza325@gmail.com>
2024-09-23 07:41:13 -0700

    Project 0x18. C - Dynamic libraries

--- 0x18-dynamic_libraries/1-create_dynamic_lib.sh
+++ 0x18-dynamic_libraries/1-create_dynamic_lib.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
-gcc -c -Wall -Werror -Wextra -pedantic -fPIC *.c
-gcc -shared -o liball.so -o *.o
+# Compile all .c files in the current directory into position-independent code (.o files)
+gcc -Wall -Werror -Wextra -pedantic -std=gnu89 -fPIC -c *.c
+
+# Create the dynamic library liball.so from all the .o files
+gcc -shared -o liball.so *.o
+
+# Clean up the object files after the dynamic library is created
+rm -f *.o
