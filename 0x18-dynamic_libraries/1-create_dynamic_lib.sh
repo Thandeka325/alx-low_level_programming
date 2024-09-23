ac0e042dbb2b0f1f65604f9c85e93c1ec125f49c
Thandeka325 <marhwayiza325@gmail.com>
2024-09-23 04:24:25 -0700

    Project 0x18. C - Dynamic libraries

--- 0x18-dynamic_libraries/1-create_dynamic_lib.sh
+++ 0x18-dynamic_libraries/1-create_dynamic_lib.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-gcc -Wall -Werror -Wextra -pedantic -std=gnu89 -fPIC -c *.c
+gcc -Wall -Wextra -pedantic -c -fPIC *.c
 gcc -shared -o liball.so *.o
 export LD_LIBRARY_PATH=.:$LD_LIBRARY_PATH
-# Clean up the object files after the dynamic library is created
-rm -f *.o
