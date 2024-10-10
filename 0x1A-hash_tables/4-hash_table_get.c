80fa60e4d2919eb5ec309e079c0663a6de832c7a
Thandeka325 <marhwayiza325@gmail.com>
2024-10-10 14:53:25 -0700

    Task 4: Get hash table

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -1,32 +1,31 @@
 #include "hash_tables.h"
+#include <stddef.h>
+#include <stdlib.h>
 #include <string.h>
 
 /**
  * hash_table_get - Retrieves a value associated with a key.
  * @ht: The hash table you want to look into.
- * @key: The key you are looking for.
+ * @key: The key to get the value of.
  *
  * Return: The value associated with the element,
  * or NULL if key couldn’t be found
  */
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
-	unsigned long int index;
 	hash_node_t *node;
+	unsigned long int index;
 
-	/* Check for invalid input */
 	if (ht == NULL || key == NULL || *key == '\0')
 		return (NULL);
-	/* Get the index for the key */
-	index = key_index((const unsigned char *)key, ht->size);
-	node = ht->array[index];
-	/* Search for the key in the linked list */
-	while (node != NULL)
-	{
-		if (strcmp(node->key, key) == 0)
-			return (node->value);
-		node = node->next;
-	}
 
-	return (NULL);
+	index = key_index((const unsigned char *)key, ht->size);
+	if (index >= ht->size)
+		return (NULL);
+
+	node = ht->array[index];
+	while (node && strcmp(node->key, key) != 0)
+		node = node->next;
+
+	return ((node == NULL) ? NULL : node->value);
 }
